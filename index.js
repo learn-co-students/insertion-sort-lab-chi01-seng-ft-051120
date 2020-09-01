dac56aaf50536c0f5ac5f2a17a34bed619a1deb8
skandow <kando1sr@gmail.com>
2020-09-01 08:27:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 function findMinAndRemove(array){
-
+    let min = array[0]
+    let index = 0
+    for (let i=1; i<array.length; i++) {
+        min = array[i] > min ? min : array[i]
+        index = array[i] > min ? index : i
+    }
+    array.splice(index, 1)
+    return min
 }
 
 function selectionSort(array){
-
+    let sortedArray = []
+    while (array.length != 0) {
+        sortedArray.push(findMinAndRemove(array))
+    }
+    return sortedArray
 }
